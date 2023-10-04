d80a5eb0f6385fdb6606d23d77db48ad7d0a6c1e
AlexandrLegchilov <Alexandr_Legchilov@epam.com>
2023-10-04 10:45:13 +0300

    debug

--- support/secretVariables.ts
+++ support/secretVariables.ts
@@ -8,9 +8,7 @@ export default class SecretVariables {
     static init(): void {
         if (!process.env.CI && !process.env.SECRET)
             throw new Error('Cannot decrypt encrypted data. Please set password as a value of SECRET environment variable.');
-        console.log('process.env.SECRET', process.env.SECRET)
         const decrypted = secureEnv({inputFile: '.env.enc', secret: process.env.SECRET as string});
-        console.log('decrypted', decrypted)
         Object.assign(process.env, decrypted);
     }
 }
